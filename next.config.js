0241e9b1c66d51aa4fd637d79c58f22e66a50cad
Darrick <z5510366@ad.unsw.edu.au>
2025-01-01 16:21:23 +1100

    modifcation

--- next.config.js
+++ next.config.js
@@ -6,8 +6,35 @@ import dotenv from 'dotenv';
 // Load environment variables
 dotenv.config();
 
+// export default {
+//   webpack: (config) => {
+//     // Map environment variables for DefinePlugin
+//     const env = Object.keys(process.env).reduce((acc, curr) => {
+//       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+//       return acc;
+//     }, {});
+
+//     // Add DefinePlugin to plugins
+//     config.plugins.push(new webpack.DefinePlugin(env));
+
+//     // Disable Webpack's filesystem cache
+//     config.cache = {
+//       type: 'memory',
+//     };
+
+//     // Adjust the devtool based on the environment
+//     if (!dev) {
+//       config.devtool = false; // Disable source maps in production
+//     } else {
+//       config.devtool = 'source-map'; // Use safer option in development
+//     }
+
+//     return config;
+//   },  
+// };
+
 export default {
-  webpack: (config) => {
+  webpack: (config, { isServer, dev }) => {
     // Map environment variables for DefinePlugin
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
@@ -30,5 +57,5 @@ export default {
     }
 
     return config;
-  },  
+  },
 };
