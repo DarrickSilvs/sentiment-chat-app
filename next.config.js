087324327420472d97b9c6e6371d2f5518f57d4c
Darrick <z5510366@ad.unsw.edu.au>
2025-01-01 16:09:41 +1100

    Modified next.config

--- next.config.js
+++ next.config.js
@@ -1,54 +1,5 @@
-// /* next.config.js */
+/* next.config.js */
     
-// import webpack from 'webpack';
-// import dotenv from 'dotenv';
-
-// // Load environment variables
-// dotenv.config();
-
-// export default {
-//   webpack: (config) => {
-//     // Map environment variables for DefinePlugin
-//     const env = Object.keys(process.env).reduce((acc, curr) => {
-//       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-//       return acc;
-//     }, {});
-
-//     // Add DefinePlugin to plugins
-//     config.plugins.push(new webpack.DefinePlugin(env));
-
-//     // Disable Webpack's filesystem cache
-//     config.cache = {
-//       type: 'memory',
-//     };
-
-//     return config;
-//   },
-
-//   async headers() {
-//     return [
-//       {
-//         // Match all routes
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'Content-Security-Policy',
-//             value: `
-//               default-src 'self';
-//               script-src 'self' 'unsafe-eval';
-//               style-src 'self' 'unsafe-inline';
-//               img-src 'self' data:;
-//               connect-src 'self' https://*.pusher.com;
-//               font-src 'self' data:;
-//               object-src 'none';
-//             `.replace(/\s{2,}/g, ' ').trim(), // Minify CSP for better performance
-//           },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
 import webpack from 'webpack';
 import dotenv from 'dotenv';
 
@@ -77,7 +28,6 @@ export default {
   async headers() {
     return [
       {
-        // Match all routes
         source: '/(.*)',
         headers: [
           {
@@ -85,15 +35,11 @@ export default {
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com;
-              img-src 'self' data:;
               connect-src 'self' https://*.pusher.com wss://*.pusher.com;
-              font-src 'self' data:;
-              object-src 'none';
-            `.replace(/\s{2,}/g, ' ').trim(), // Minify CSP for better performance
+            `.replace(/\s{2,}/g, ' ').trim(),
           },
         ],
       },
     ];
-  },
+  },  
 };
