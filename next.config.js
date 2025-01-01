18545e11761ad2fde0ce89e4d03b733de0316247
Darrick <z5510366@ad.unsw.edu.au>
2025-01-01 16:41:20 +1100

    restore

--- next.config.js
+++ next.config.js
@@ -6,35 +6,8 @@ import dotenv from 'dotenv';
 // Load environment variables
 dotenv.config();
 
-// export default {
-//   webpack: (config) => {
-//     // Map environment variables for DefinePlugin
-//     const env = Object.keys(process.env).reduce((acc, curr) => {
-//       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-//       return acc;
-//     }, {});
-
-//     // Add DefinePlugin to plugins
-//     config.plugins.push(new webpack.DefinePlugin(env));
-
-//     // Disable Webpack's filesystem cache
-//     config.cache = {
-//       type: 'memory',
-//     };
-
-//     // Adjust the devtool based on the environment
-//     if (!dev) {
-//       config.devtool = false; // Disable source maps in production
-//     } else {
-//       config.devtool = 'source-map'; // Use safer option in development
-//     }
-
-//     return config;
-//   },  
-// };
-
 export default {
-  webpack: (config, { isServer, dev }) => {
+  webpack: (config) => {
     // Map environment variables for DefinePlugin
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
@@ -44,26 +17,11 @@ export default {
     // Add DefinePlugin to plugins
     config.plugins.push(new webpack.DefinePlugin(env));
 
-    // Add polyfills for Pusher WebSocket (if needed)
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false, // Avoid issues with fs module on the client
-      };
-    }
-
     // Disable Webpack's filesystem cache
     config.cache = {
       type: 'memory',
     };
 
-    // Adjust the devtool based on the environment
-    if (!dev) {
-      config.devtool = false; // Disable source maps in production
-    } else {
-      config.devtool = 'source-map'; // Use safer option in development
-    }
-
     return config;
-  },
+  },  
 };
